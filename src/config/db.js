8d3f5a3140c9458b26db38a091ac06941c73bbeb
dk041102 <dk107175@gmail.com>
2026-05-08 02:53:32 +0530

    Fix : revert to MONGO_URI

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URI);
-  console.log("Connecting to:", process.env.MONGODB_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log("Connecting to:", process.env.MONGO_URI);
   console.log(`MongoDB: ${conn.connection.host}`);
 };
 module.exports = connectDB;
